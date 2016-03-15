8f5877d1332b3bf4afec5200bf4656ca7fe38d53
igormigunov <fear9080@mail.ru>
2016-03-15 21:16:36 +0300

    add left panel and pager

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function (e) {
     nice = $("#userList").niceScroll();
-
+    $('.right-panel > a').each(function(e){
+        $tooltip=$(this).tooltip(
+            {
+                html: true,
+                title: $('#tooltip').html(),
+                placement:'right',
+                animation: true,
+                delay: {show: 0, hide: 200}
+            })
+    });
 });
